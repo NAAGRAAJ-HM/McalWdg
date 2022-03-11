0d5ccd127c2487b7f13bb47dc03e6cc4242532a5
root <nagaraja.hm.0001@gmail.com>
2022-03-11 12:12:51 +0100

    Update: AUTOSAR basic software micro-controller abstraction module Wdg

--- LATEST/infWdg_SchM.h
+++ LATEST/infWdg_SchM.h
@@ -1,15 +1,13 @@
+#pragma once
 /*****************************************************/
-/* File   : Wdg.cpp                                  */
+/* File   : infWdg_SchM.h                            */
 /* Author : Naagraaj HM                              */
 /*****************************************************/
 
 /*****************************************************/
 /* #INCLUDES                                         */
 /*****************************************************/
-#include "module.h"
-#include "infWdg_EcuM.h"
-#include "infWdg_Dcm.h"
-#include "infWdg_SchM.h"
+#include "Compiler_Cfg_Wdg.h"
 
 /*****************************************************/
 /* #DEFINES                                          */
@@ -22,15 +20,6 @@
 /*****************************************************/
 /* TYPEDEFS                                          */
 /*****************************************************/
-class module_Wdg:
-      public abstract_module
-{
-   public:
-      FUNC(void, WDG_CODE) InitFunction   (void);
-      FUNC(void, WDG_CODE) DeInitFunction (void);
-      FUNC(void, WDG_CODE) GetVersionInfo (void);
-      FUNC(void, WDG_CODE) MainFunction   (void);
-};
 
 /*****************************************************/
 /* CONSTS                                            */
@@ -43,36 +32,11 @@ class module_Wdg:
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-module_Wdg     Wdg;
-infEcuMClient* gptrinfEcuMClient_Wdg = &Wdg;
-infDcmClient*  gptrinfDcmClient_Wdg  = &Wdg;
-infSchMClient* gptrinfSchMClient_Wdg = &Wdg;
+extern infSchMClient* gptrinfSchMClient_Wdg;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-FUNC(void, WDG_CODE) module_Wdg::InitFunction(void){
-}
-
-FUNC(void, WDG_CODE) module_Wdg::DeInitFunction(void){
-}
-
-FUNC(void, WDG_CODE) module_Wdg::GetVersionInfo(void){
-}
-
-FUNC(void, WDG_CODE) module_Wdg::MainFunction(void){
-}
-
-#include "Wdg_Unused.h"
-
-FUNC(void, WDG_CODE) class_Wdg_Unused::SetMode(void){
-}
-
-FUNC(void, WDG_CODE) class_Wdg_Unused::SetTriggerCondition(void){
-}
-
-FUNC(void, WDG_CODE) class_Wdg_Unused::Cbk_GptNotification(void){
-}
 
 /*****************************************************/
 /* EOF                                               */
