0989ddaddb6238a4decf6813df21c85bfda0dc87
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-12-27 20:40:48 +0100

    Update: AUTOSAR basic software micro-controller abstraction driver module  for watchdog stack

---
+++ InfineonTLE9879QXA40_R2023/McalWdg.cpp
@@ -0,0 +1,164 @@
+/******************************************************************************/
+/* File   : McalWdg.cpp                                                       */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "Module.hpp"
+#include "McalWdg.hpp"
+#include "infMcalWdg_Imp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define MCALWDG_AR_RELEASE_VERSION_MAJOR                                       4
+#define MCALWDG_AR_RELEASE_VERSION_MINOR                                       3
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+#if(MCALWDG_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible MCALWDG_AR_RELEASE_VERSION_MAJOR!"
+#endif
+
+#if(MCALWDG_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible MCALWDG_AR_RELEASE_VERSION_MINOR!"
+#endif
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+VAR(module_McalWdg, MCALWDG_VAR) McalWdg;
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+FUNC(void, MCALWDG_CODE) module_McalWdg::InitFunction(
+      CONSTP2CONST(ConstModule_TypeAbstract, MCALWDG_CONST,       MCALWDG_APPL_CONST) lptrConstModule
+   ,  CONSTP2CONST(CfgModule_TypeAbstract,   MCALWDG_CONFIG_DATA, MCALWDG_APPL_CONST) lptrCfgModule
+){
+#if(STD_ON == McalWdg_InitCheck)
+   if(
+         E_OK
+      != IsInitDone
+   ){
+#endif
+      if(
+            (NULL_PTR != lptrConstModule)
+         && (NULL_PTR != lptrCfgModule)
+      ){
+         lptrConst = (const ConstMcalWdg_Type*)lptrConstModule;
+         lptrCfg   = lptrCfgModule;
+      }
+      else{
+#if(STD_ON == McalWdg_DevErrorDetect)
+         ServiceDet_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
+#if(STD_ON == McalWdg_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == McalWdg_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  MCALWDG_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, MCALWDG_CODE) module_McalWdg::DeInitFunction(
+   void
+){
+#if(STD_ON == McalWdg_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == McalWdg_InitCheck)
+      IsInitDone = E_NOT_OK;
+   }
+   else{
+#if(STD_ON == McalWdg_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  MCALWDG_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+#include "wdt1.hpp"
+FUNC(void, MCALWDG_CODE) module_McalWdg::MainFunction(
+   void
+){
+#if(STD_ON == McalWdg_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+
+   (void)WDT1_Service();
+
+#if(STD_ON == McalWdg_InitCheck)
+   }
+   else{
+#if(STD_ON == McalWdg_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  MCALWDG_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, MCALWDG_CODE) module_McalWdg::SetMode(
+   void
+){
+}
+
+FUNC(void, MCALWDG_CODE) module_McalWdg::SetTriggerCondition(
+   void
+){
+}
+
+FUNC(void, MCALWDG_CODE) module_McalWdg::Cbk_McalGptNotification(
+   void
+){
+}
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
---
+++ InfineonTLE9879QXA40_R2023/wdt1.cpp
@@ -0,0 +1,144 @@
+/******************************************************************************/
+/* File   : wdt1.cpp                                                          */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "types.hpp"
+
+#include "uC_Scu.hpp"
+
+#include "wdt1.hpp"
+
+#include "scu_defines.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define SYSTICK_DELAY_THRESHOLD 100U
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+       uint32 WD_Counter;
+static bool   bSOWactive;
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+/*
+void WDT1_Init(void){
+   SCUPM.WDT1_TRIG.reg = (uint8) SCUPM_WDT1_TRIG;
+   WD_Counter = 0u;
+   bSOWactive = false;
+}
+
+void SysTick_Init(void){
+   CPU.SYSTICK_RL.reg = (uint32)SysTickRL;
+   CPU.SYSTICK_CUR.reg = 0u;
+   CPU.SYSTICK_CS.bit.CLKSOURCE = 1u;
+   CPU.SYSTICK_CS.bit.TICKINT = 1u;
+   CPU.SYSTICK_CS.bit.ENABLE = 1u;
+}
+
+void WDT1_Stop(void){
+   CPU.SYSTICK_CS.bit.ENABLE = 0u;
+}
+*/
+bool WDT1_Service(void){
+   bool bResult = false;
+
+   if(
+         (
+               (uint32)SCUPM_WDT1_TRIGGER
+            <  WD_Counter
+         )
+      || (
+               true
+            == bSOWactive
+         )
+   ){
+      SCUPM.WDT1_TRIG.reg = (uint8) SCUPM_WDT1_TRIG;
+      WD_Counter          = 0u;
+      bSOWactive          = false;
+      bResult             = true;
+   }
+
+   return(bResult);
+}
+/*
+void WDT1_SOW_Service(uint32 NoOfSOW){
+   SCUPM.WDT1_TRIG.reg = (NoOfSOW & 3u) << 6u;
+   bSOWactive = true;
+}
+
+void Delay_us(uint32 delay_time_us){
+   uint32 systick_target_val;
+   uint32 systick_val;
+   uint32 delay_count;
+   uint32 systick_cur;
+   uint32 systick_rl;
+   systick_rl = SysTick_ReloadValue_Get();
+   do{
+      systick_val = SysTick_Value_Get();
+   }while((systick_val < SYSTICK_DELAY_THRESHOLD) || (systick_val > (systick_rl - SYSTICK_DELAY_THRESHOLD)));
+   while(delay_time_us >= (uint32)1000){
+      while(SysTick_Value_Get() < systick_val){}
+      while(SysTick_Value_Get() > systick_val){}
+      delay_time_us -= (uint32)1000;
+   }
+   delay_count = delay_time_us * (uint32)One_us;
+   systick_val = SysTick_Value_Get();
+   if( systick_val >= delay_count){
+      systick_target_val = systick_val - delay_count;
+      do{
+         systick_cur = SysTick_Value_Get();
+      }
+      while((systick_cur > systick_target_val) && (systick_cur < systick_val));
+   }
+   else{
+      systick_target_val = systick_rl - ( delay_count - systick_val );
+      while(SysTick_Value_Get() < systick_val){}
+      while(SysTick_Value_Get() > systick_target_val){}
+   }
+   return;
+}
+
+void WDT1_Window_Count(void){
+  WD_Counter++;
+}
+
+uint32 SysTick_Value_Get(void){
+   return CPU.SYSTICK_CUR.reg;
+}
+
+uint32 SysTick_ReloadValue_Get(void){
+   return(CPU.SYSTICK_RL.reg);
+}
+
+void SysTick_ReloadValue_Set(uint32 val){
+  CPU.SYSTICK_RL.reg = val;
+}
+*/
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
