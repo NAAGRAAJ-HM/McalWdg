f9a3bf74ab05aa5ef2502ee48a1a6d20fd561a7b
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:52:12 +0100

    Update: AUTOSAR basic software micro-controller abstraction driver module  for watchdog stack

---
+++ InfineonTLE9879QXA40_R2023/infMcalWdg_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infMcalWdg_ServiceNvM.hpp                                         */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTMCALWDG_AR_RELEASE_VERSION_MAJOR                                  4
+#define CONSTMCALWDG_AR_RELEASE_VERSION_MINOR                                  3
+
+#define Const_DefaultInit_ConstMcalWdg                                                                           \
+   ,  {CONSTMCALWDG_AR_RELEASE_VERSION_MAJOR, CONSTMCALWDG_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
