8aef88135124e4bbc1f2c549c47fb987313b9496
Raajnaag HULIYAPURADA MATA <nagaraja.hm.0001@gmail.com>
2023-06-17 21:22:00 +0200

    Initial commit

---
+++ RenesasRH850F1LR7F7010133AFP_R2023/McalWdg.c
@@ -0,0 +1,94 @@
+/******************************************************************************/
+/* File   : McalWdg.c                                                         */
+/*                                                                            */
+/* Author : Raajnaag HULIYAPURADA MATA                                        */
+/*                                                                            */
+/* License / Warranty / Terms and Conditions                                  */
+/*                                                                            */
+/* Everyone is permitted to copy and distribute verbatim copies of this lice- */
+/* nse document, but changing it is not allowed. This is a free, copyright l- */
+/* icense for software and other kinds of works. By contrast, this license is */
+/* intended to guarantee your freedom to share and change all versions of a   */
+/* program, to make sure it remains free software for all its users. You have */
+/* certain responsibilities, if you distribute copies of the software, or if  */
+/* you modify it: responsibilities to respect the freedom of others.          */
+/*                                                                            */
+/* All rights reserved. Copyright © 1982 Raajnaag HULIYAPURADA MATA           */
+/*                                                                            */
+/* Always refer latest software version from:                                 */
+/* https://github.com/RaajnaagHuliyapuradaMata?tab=repositories               */
+/*                                                                            */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "Std_Types.h"
+
+#include "CfgMcalWdg.h"
+#include "infMcalWdgSwcServiceEcuM.h"
+#include "uC_Wdg.h"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+FUNC(void, MCALWDG_CODE) infMcalWdg_vInitWdg0(void){
+   WDTA0.MD = (uint8)0x47;
+}
+
+FUNC(void, MCALWDG_CODE) infMcalWdg_vInitWdg1(void){
+   WDTA1.MD = (uint8)0x07;
+}
+
+#ifdef FEATURE_WdgShowOnTP2
+static FUNC(void, MCALWDG_CODE) vShowTrigger(void){
+   static uint8 u8Count;
+   if(++u8Count & 0x01) TP2(1);
+   else                 TP2(0);
+}
+#endif
+
+FUNC(void, MCALWDG_CODE) infMcalWdg_vTriggerWdg0(void){
+   WDTA0.WDTE = (uint8)0xAC;
+#ifdef FEATURE_WdgShowOnTP2
+   vShowTrigger();
+#endif
+}
+
+FUNC(void, MCALWDG_CODE) infMcalWdg_vTriggerWdg1(void){
+   WDTA1.WDTE = (uint8)0xAC;
+}
+
+FUNC(void, MCALWDG_CODE) infMcalWdgSwcServiceEcuM_vInitFunction(void){
+   infMcalWdg_vInitWdg0();
+   infMcalWdg_vTriggerWdg0();
+}
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
