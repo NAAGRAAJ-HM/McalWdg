4b0b5e026fe512a24ce03b4c906d4d4abacf5531
Raajnaag HULIYAPURADA MATA <nagaraja.hm.0001@gmail.com>
2023-07-18 15:36:07 +0200

    Initial commit

--- RenesasRH850F1LR7F7010133AFP_R2023/infMcalWdgSwcApplStartUp.h
+++ RenesasRH850F1LR7F7010133AFP_R2023/infMcalWdgSwcApplStartUp.h
@@ -1,5 +1,6 @@
+#pragma once
 /******************************************************************************/
-/* File   : McalWdg.c                                                         */
+/* File   : infMcalWdgSwcApplStartUp.h                                        */
 /*                                                                            */
 /* Author : Raajnaag HULIYAPURADA MATA                                        */
 /*                                                                            */
@@ -23,14 +24,7 @@
 /******************************************************************************/
 /* #INCLUDES                                                                  */
 /******************************************************************************/
-#include "Std_Types.h"
-
-#include "CfgMcalWdg.h"
-
-#include "infMcalWdgSwcServiceEcuM.h"
-#include "infMcalWdgSwcApplStartUp.h"
-
-#include "uC_Wdg.h"
+#include "CompilerCfg_McalWdg.h"
 
 /******************************************************************************/
 /* #DEFINES                                                                   */
@@ -55,57 +49,13 @@
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
-static boolean McalWdg_bFlagInit;
 
 /******************************************************************************/
 /* FUNCTIONS                                                                  */
 /******************************************************************************/
-FUNC(void, MCALWDG_CODE) infMcalWdg_vInitWdg0(void){
-   WDTA0.MD = (uint8)0x47;
-}
-
-FUNC(void, MCALWDG_CODE) infMcalWdg_vInitWdg1(void){
-   WDTA1.MD = (uint8)0x07;
-}
-
-#ifdef FEATURE_WdgShowOnTP2
-static FUNC(void, MCALWDG_CODE) vShowTrigger(void){
-   static uint8 u8Count;
-   if(++u8Count & 0x01) TP2(1);
-   else                 TP2(0);
-}
-#endif
-
-FUNC(void, MCALWDG_CODE) infMcalWdg_vTriggerWdg0(void){
-   WDTA0.WDTE = (uint8)0xAC;
-#ifdef FEATURE_WdgShowOnTP2
-   vShowTrigger();
-#endif
-}
-
-FUNC(void, MCALWDG_CODE) infMcalWdg_vTriggerWdg1(void){
-   WDTA1.WDTE = (uint8)0xAC;
-}
-
-FUNC(void, MCALWDG_CODE) infMcalWdgSwcApplStartUp_vClrFlagInit(void){
-   McalWdg_bFlagInit = FALSE;
-}
-
-FUNC(void, MCALWDG_CODE) infMcalWdgSwcApplStartUp_vSetFlagInit(void){
-   McalWdg_bFlagInit = TRUE;
-}
-
-FUNC(boolean, MCALWDG_CODE) infMcalWdgSwcApplStartUp_bGetFlagInit(void){
-   return(
-         FALSE
-      != McalWdg_bFlagInit
-   );
-}
-
-FUNC(void, MCALWDG_CODE) infMcalWdgSwcServiceEcuM_vInitFunction(void){
-   infMcalWdg_vInitWdg0();
-   infMcalWdg_vTriggerWdg0();
-}
+extern FUNC(void,    MCALWDG_CODE) infMcalWdgSwcApplStartUp_vClrFlagInit(void);
+extern FUNC(void,    MCALWDG_CODE) infMcalWdgSwcApplStartUp_vSetFlagInit(void);
+extern FUNC(boolean, MCALWDG_CODE) infMcalWdgSwcApplStartUp_bGetFlagInit(void);
 
 /******************************************************************************/
 /* EOF                                                                        */
